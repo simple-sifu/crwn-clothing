5dd806b0ffa944ad955e6e623aac4ce2808501f0
simple-sifu <kattyntommy@gmail.com>
2020-10-02 03:44:40 -0400

    broke up html into smaller components

---
+++ src/components/directory.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import MenuItem from './menu-items'
+import "./directory.scss"
+
+const Directory = () => (
+    
+        <div className='directory-menu'>
+            <MenuItem title={"HATS"} />    
+            <MenuItem title={"JACKETS"} />   
+            <MenuItem title={"SNEAKERS"} />   
+            <MenuItem title={"WOMENS"} />          
+            <MenuItem title={"MENS"} />                 
+        </div>
+
+)
+
+export default Directory;

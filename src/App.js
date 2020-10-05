2d9ab3cf5c5afe906e2a48ff3eec5af033e36d9b
simple-sifu <kattyntommy@gmail.com>
2020-10-05 12:40:35 -0400

    refactor

--- src/App.js
+++ src/App.js
@@ -3,39 +3,14 @@ import { Route} from 'react-router-dom';
 
 import HomePage from './pages/HomePage';
 import './App.css';
-
-
-const TopicsList = () => {
-  return (
-    <div>
-      <h1>TOPIC LIST PAGE</h1>
-    </div>
-  )
-};
-
-const TopicsDetail = () => {
-  return (
-    <div>
-      <h1>TOPIC DETAIL PAGE</h1>
-    </div>
-  )
-};
-const HatsPage = () => {
-  return (
-    <div>
-      <h1>HATS Page</h1>
-    </div>
-  )
-};
-
+import ShopPage from './pages/ShopPage';
 
 const App = () => {
   return (
     <div>
         <Route exact path='/' component={HomePage} />
-        <Route exact path='/hats' component={HatsPage} />
-        <Route exact path='/topics' component={TopicsList} />
-        <Route path='/topics/:topicId' component={TopicsDetail} />
+        <Route exact path='/shop' component={ShopPage} />
+
 
     </div>
 
